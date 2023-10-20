d737e3fb8acbb4d9b96eabc3e1b2c1c0d1d3ad86
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-10-20 16:47:53 +0300

    rewrited main logic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use std::{fs::{read_to_string, write}, collections::HashMap};
-use serde_json::{Result, Value};
+use serde_json::{Result as JsonResult, Value};
 use regex::Regex;
-use reducer::{ReducerConfig, Reducer};
-
-mod reducer;
 
 struct Config {
     ctx_path: Option<String>,
@@ -19,6 +16,12 @@ impl Default for Config {
             dest: Some(String::from("./index-aliased.html")),
         }
     }
+}
+
+#[derive(Debug)]
+enum AliasError {
+    InvalidAlias(String),
+    InvalidContext(String),
 }
 
 fn main() {
@@ -47,18 +50,16 @@ fn alias8n(config: Option<Config>) {
     let src_string = read_to_string(&source).unwrap();
 
     let re = Regex::new(r"a\(:.*?:\)").unwrap();
-
     let aliases = re.captures_iter(&src_string);
 
-    let mut reducer = Reducer::new(ReducerConfig {
-        src_string: src_string.to_owned(),
-        ctx: json_value_into_hashmap(ctx),
-        aliases: captures_into_vec(aliases),
-    });
+    let aliases = captures_into_vec(aliases);
 
-    let aliased_src_string = reducer.init();
+
+
+    let aliased_src_string = replace_aliases(&src_string, &aliases, ctx);
     
     write(dest, aliased_src_string).unwrap();
+
 }
 
 fn try_exists(path: &str) -> bool{
@@ -77,20 +78,48 @@ fn captures_into_vec(captures: regex::CaptureMatches) -> Vec<String> {
     vec
 }
 
-fn json_value_into_hashmap(value: Value) -> HashMap<String, Value> {
-    let mut map = HashMap::new();
-    match value {
-        Value::Object(obj) => {
-            for (key, value) in obj {
-                map.insert(key, value);
-            }
+fn replace_aliases(src_string: &str, aliases: &Vec<String>, ctx: Value) -> String {
+    let mut result_string = String::from(src_string);
+    for alias in aliases {
+        let processed = process_alias(alias, &ctx);
+        match processed {
+            Ok(processed) => {
+                println!("{:#?}, {}", processed, "Hello world!");
+            },
+            Err(e) => {
+                println!("{:?}: {}", e, alias);
+            },
         }
-        Value::Array(arr) => {
-            for (index, value) in arr.iter().enumerate() {
-                map.insert(index.to_string(), value.clone());
-            }
-        }
-        _ => {}
     }
-    map
+    String::from("Hello")
+}
+
+fn process_alias(alias: &str, ctx: &Value) -> Result<String, AliasError> {
+    let raw_alias = alias.replace("a(:", "").replace(":)", "");
+
+    let arguments = raw_alias.split(",").collect::<Vec<&str>>();
+
+    let nesting = arguments.get(0).unwrap().split(".").collect::<Vec<&str>>();
+    
+    let value_from_ctx = get_value_from_ctx(nesting, ctx)?;
+    Ok(value_from_ctx)
+}
+
+fn get_value_from_ctx(nesting: Vec<&str>, ctx: &Value) -> Result<String, AliasError> {
+    let mut value = ctx;
+    for nest in nesting {
+        value = match value.get(nest) {
+            Some(v) => v,
+            None => {
+                return Err(AliasError::InvalidAlias(String::from("Invalid alias")));
+            },
+        };
+    }
+    
+    if value.is_string() { // TODO: value can be a number, bool, string,
+        let value = value.as_str().ok_or(AliasError::InvalidAlias(String::from("Invalid alias value")))?;
+        return Ok(value[0..value.len()].to_string());
+    } else {
+        return Err(AliasError::InvalidContext(String::from("Invalid context")));
+    }
 }
