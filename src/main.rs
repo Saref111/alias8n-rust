7303f4c6fd5dcadee720a08d326c0db8f0761f54
Ivan Borovyk <Ivan_Borovyk@epam.com>
2023-11-23 08:17:54 +0200

    refactored

--- src/main.rs
+++ src/main.rs
@@ -1,136 +1,126 @@
-use std::fs::{read_to_string, write};
-use serde_json::Value;
-use regex::Regex;
+use crate::alias8n::{Config, alias8n};
 
-struct Config {
-    ctx_path: Option<String>,
-    source: Option<String>,
-    dest: Option<String>,
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Config {
-            ctx_path: Some(String::from("./ctx.json")),
-            source: Some(String::from("./index.html")),
-            dest: Some(String::from("./index-aliased.html")),
-        }
-    }
-}
-
-#[derive(Debug)]
-enum AliasError {
-    InvalidAlias(String),
-    InvalidContext(String),
-}
+mod alias8n;
+mod lib;
 
 fn main() {
-    alias8n(None);	
+    // alias8n --ctx ./ctx.json --source ./index.html --dest ./index-aliased.html
+    // alias8n --ctx ./ctx.json --source ./index.html
+    // alias8n --ctx ./ctx.json
+    // alias8n
+    let args: Vec<String> = std::env::args().collect();
+
+    let config = get_config_from_args(args);
+    alias8n(config);	
 }
 
-fn alias8n(config: Option<Config>) {
-    let config = config.unwrap_or_default();
-    let ctx_path = config.ctx_path.unwrap();
-    let source = config.source.unwrap();
-    let dest = config.dest.unwrap();
-
-    if !ctx_path.ends_with(".json") {
-        panic!("Context file should be a json file");
-    }
-    if !try_exists(&ctx_path) {
-        panic!("No context file found by given path");
-    }       
-    if !try_exists(&source) {
-        panic!("No source file found by given path");
-    }
-
-    let ctx = read_to_string(&ctx_path).unwrap();
-    let ctx: Value = serde_json::from_str(&ctx).unwrap();
-    let src_string = read_to_string(&source).unwrap();
-
-    let re = Regex::new(r"a\(:.*?:\)").unwrap();
-    let aliases = re.captures_iter(&src_string);
-
-    let aliases = captures_into_vec(aliases);
-
-
-
-    let aliased_src_string = replace_aliases(&src_string, &aliases, ctx);
-    
-    write(dest, aliased_src_string).unwrap();
-
-}
-
-fn try_exists(path: &str) -> bool{
-    let result = std::fs::metadata(path);
-    match result {
-        Ok(_) => true,
-        Err(_) => false,
-    }
-}
-
-fn captures_into_vec(captures: regex::CaptureMatches) -> Vec<String> {
-    let mut vec = Vec::new();
-    for capture in captures {
-        vec.push(capture.get(0).unwrap().as_str().to_string());
-    }
-    vec
-}
-
-fn replace_aliases(src_string: &str, aliases: &Vec<String>, ctx: Value) -> String {
-    let mut result_string = String::from(src_string);
-    for alias in aliases {
-        let processed = process_alias(alias, &ctx);
-        match processed {
-            Ok(processed) => {
-                result_string = result_string.replace(alias, &processed);
+fn get_config_from_args(args: Vec<String>) -> Option<Config> {
+    let mut config = Config::default();
+    let mut i = 1;
+    while i < args.len() {
+        let arg = args.get(i).unwrap();
+        match arg.as_str() {
+            "--ctx" => {
+                i += 1;
+                config.ctx_path = Some(args.get(i).unwrap().to_string());
             },
-            Err(e) => {
-                println!("{:?}: {}", e, alias);
+            "--source" => {
+                i += 1;
+                config.source = Some(args.get(i).unwrap().to_string());
+            },
+            "--dest" => {
+                i += 1;
+                config.dest = Some(args.get(i).unwrap().to_string());
+            },
+            _ => {
+                panic!("Invalid argument");
             },
         }
+        i += 1;
     }
-    result_string
+    Some(config)
 }
 
-fn process_alias(alias: &str, ctx: &Value) -> Result<String, AliasError> {
-    let raw_alias = alias.replace("a(:", "").replace(":)", "");
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    let arguments = raw_alias.split(",").collect::<Vec<&str>>();
-
-    
-
-    let nesting = arguments.get(0).unwrap().split(".").collect::<Vec<&str>>();
-    
-    let mut value_from_ctx = get_value_from_ctx(nesting, ctx)?;
-
-    if arguments.len() > 1 {
-        for (i, arg) in arguments[1..].iter().enumerate() {
-            value_from_ctx = value_from_ctx.replace(&format!("<{}>", i + 1), arg.trim());
-        }
+    #[test]
+    fn test_get_config_from_args() {
+        let args = vec![
+            String::from("alias8n"),
+            String::from("--ctx"),
+            String::from("./ctx.json"),
+            String::from("--source"),
+            String::from("./index.html"),
+            String::from("--dest"),
+            String::from("./index-aliased.html"),
+        ];
+        let config = get_config_from_args(args).unwrap();
+        assert_eq!(config.ctx_path.unwrap(), "./ctx.json");
+        assert_eq!(config.source.unwrap(), "./index.html");
+        assert_eq!(config.dest.unwrap(), "./index-aliased.html");
     }
 
-    Ok(value_from_ctx)
-}
-
-fn get_value_from_ctx(nesting: Vec<&str>, ctx: &Value) -> Result<String, AliasError> {
-    let mut value = ctx;
-    for nest in nesting {
-        value = match value.get(nest) {
-            Some(v) => v,
-            None => {
-                return Err(AliasError::InvalidAlias(String::from("Invalid alias")));
-            },
-        };
+    #[test]
+    fn test_get_config_from_args_with_default() {
+        let args = vec![
+            String::from("alias8n"),
+            String::from("--ctx"),
+            String::from("./ctx.json"),
+        ];
+        let config = get_config_from_args(args).unwrap();
+        assert_eq!(config.ctx_path.unwrap(), "./ctx.json");
+        assert_eq!(config.source.unwrap(), "./index.html");
+        assert_eq!(config.dest.unwrap(), "./index-aliased.html");
     }
-    
-    if value.is_string() { // TODO: think about array handling
-        let value = value.as_str().ok_or(AliasError::InvalidAlias(String::from("Invalid alias value")))?;
-        return Ok(value[0..value.len()].to_string());
-    } else if value.is_boolean() || value.is_number() {
-        let value = value.to_string();
-        return Ok(value);
-    } else {
-        return Err(AliasError::InvalidContext(String::from("Invalid context")));
+
+    #[test]
+    fn test_get_config_from_args_with_default_2() {
+        let args = vec![
+            String::from("alias8n"),
+            String::from("--ctx"),
+            String::from("./ctx.json"),
+            String::from("--source"),
+            String::from("./index.html"),
+        ];
+        let config = get_config_from_args(args).unwrap();
+        assert_eq!(config.ctx_path.unwrap(), "./ctx.json");
+        assert_eq!(config.source.unwrap(), "./index.html");
+        assert_eq!(config.dest.unwrap(), "./index-aliased.html");
+    }
+
+    #[test]
+    fn test_get_config_from_args_with_default_3() {
+        let args = vec![
+            String::from("alias8n"),
+            String::from("--ctx"),
+            String::from("./ctx.json"),
+            String::from("--dest"),
+            String::from("./index-aliased.html"),
+        ];
+        let config = get_config_from_args(args).unwrap();
+        assert_eq!(config.ctx_path.unwrap(), "./ctx.json");
+        assert_eq!(config.source.unwrap(), "./index.html");
+        assert_eq!(config.dest.unwrap(), "./index-aliased.html");
+    }
+
+    #[test]
+    fn test_get_config_from_args_with_default_4() {
+        let args = vec![
+            String::from("alias8n"),
+            String::from("--ctx"),
+            String::from("./ctx.json"),
+            String::from("--source"),
+            String::from("./index.html"),
+            String::from("--dest"),
+            String::from("./index-aliased.html"),
+            String::from("--ctx"),
+            String::from("./ctx2.json"),
+        ];
+        let config = get_config_from_args(args).unwrap();
+        assert_eq!(config.ctx_path.unwrap(), "./ctx2.json");
+        assert_eq!(config.source.unwrap(), "./index.html");
+        assert_eq!(config.dest.unwrap(), "./index-aliased.html");
     }
 }
